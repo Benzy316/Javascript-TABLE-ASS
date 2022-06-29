ffef68f4fbd7edf3ee6dfca403e89e67fd0ebc39
Benzy316 <101456183+Benzy316@users.noreply.github.com>
2022-06-29 15:46:44 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,101 @@
+const headers = ["WORKERS","N0.","FirstName","LastName","Age","Sex","Position","Date_Of_Employment",]
+
+document.getElementById("ta").innerHTML = headers[0]
+document.getElementById("t1").innerHTML = headers[1]
+document.getElementById("t2").innerHTML = headers[2]
+document.getElementById("t3").innerHTML = headers[3]
+document.getElementById("t4").innerHTML = headers[4]
+document.getElementById("t5").innerHTML = headers[5]
+document.getElementById("t6").innerHTML = headers[6]
+document.getElementById("t7").innerHTML = headers[7]
+
+const title = [{NUM:[1,2,3,4,5,6,7,8,9,10]},
+{FN:["Thompson", "Johnson", "Chisom", "jide", "Williams", "Nancy", "Nicholas", "Ahmed", "Halima","Richard"]},
+{LA:["Anthony","Mary","Onyeka","Olabisi","Ajayi","Chukwuemeka","Uchemba","Olayinka","Mohammed","Adeyemi"]},
+{AGE:[32,23,28,29,30,27,26,24,26,41]},
+{SEX:["Male","Female"]},
+{POS:["Head Of Finance Department","Software Engineer","Human Resource Manager","Asistant Head (Software Department)",
+"Head Of Software Department","Secretary","Graphic Designer","Senior Finance Manager","C.E.O"]},
+{DOE:["17/07/2014","05/03/2019", "21/06/2016","05/06/2016","06/09/2015",
+"23/08/2016","22/02/2020","15/08/2020","30/05/2018","17/11/2010"]}
+]
+
+document.getElementById("a1").innerHTML = title[0].NUM[0]
+document.getElementById("a2").innerHTML = title[1].FN[0]
+document.getElementById("a3").innerHTML = title[2].LA[0]
+document.getElementById("a4").innerHTML = title[3].AGE[0]
+document.getElementById("a5").innerHTML =  title[4].SEX[0]
+document.getElementById("a6").innerHTML = title[5].POS[0]
+document.getElementById("a7").innerHTML = title[6].DOE[0]
+
+document.getElementById("b1").innerHTML = title[0].NUM[1]
+document.getElementById("b2").innerHTML = title[1].FN[1]
+document.getElementById("b3").innerHTML = title[2].LA[1]
+document.getElementById("b4").innerHTML = title[3].AGE[1]
+document.getElementById("b5").innerHTML = title[4].SEX[1]
+document.getElementById("b6").innerHTML = title[5].POS[1]
+document.getElementById("b7").innerHTML = title[6].DOE[1]
+
+document.getElementById("c1").innerHTML = title[0].NUM[2]
+document.getElementById("c2").innerHTML = title[1].FN[2]
+document.getElementById("c3").innerHTML = title[2].LA[2]
+document.getElementById("c4").innerHTML = title[3].AGE[2]
+document.getElementById("c5").innerHTML = title[4].SEX[1]
+document.getElementById("c6").innerHTML = title[5].POS[2]
+document.getElementById("c7").innerHTML = title[6].DOE[2]
+
+document.getElementById("d1").innerHTML = title[0].NUM[3]
+document.getElementById("d2").innerHTML = title[1].FN[3]
+document.getElementById("d3").innerHTML = title[2].LA[3]
+document.getElementById("d4").innerHTML = title[3].AGE[3]
+document.getElementById("d5").innerHTML = title[4].SEX[0]
+document.getElementById("d6").innerHTML = title[5].POS[3]
+document.getElementById("d7").innerHTML = title[6].DOE[3]
+
+document.getElementById("e1").innerHTML = title[0].NUM[4]
+document.getElementById("e2").innerHTML = title[1].FN[4]
+document.getElementById("e3").innerHTML = title[2].LA[4]
+document.getElementById("e4").innerHTML = title[3].AGE[4]
+document.getElementById("e5").innerHTML = title[4].SEX[0]
+document.getElementById("e6").innerHTML = title[5].POS[4]
+document.getElementById("e7").innerHTML = title[6].DOE[4]
+
+document.getElementById("f1").innerHTML = title[0].NUM[5]
+document.getElementById("f2").innerHTML = title[1].FN[5]
+document.getElementById("f3").innerHTML = title[2].LA[5]
+document.getElementById("f4").innerHTML = title[3].AGE[5]
+document.getElementById("f5").innerHTML = title[4].SEX[1]
+document.getElementById("f6").innerHTML = title[5].POS[5]
+document.getElementById("f7").innerHTML = title[6].DOE[5]
+
+document.getElementById("g1").innerHTML = title[0].NUM[6]
+document.getElementById("g2").innerHTML = title[1].FN[6]
+document.getElementById("g3").innerHTML = title[2].LA[6]
+document.getElementById("g4").innerHTML = title[3].AGE[6]
+document.getElementById("g5").innerHTML = title[4].SEX[0]
+document.getElementById("g6").innerHTML = title[5].POS[1]
+document.getElementById("g7").innerHTML = title[6].DOE[6]
+
+document.getElementById("h1").innerHTML = title[0].NUM[7]
+document.getElementById("h2").innerHTML = title[1].FN[7]
+document.getElementById("h3").innerHTML = title[2].LA[7]
+document.getElementById("h4").innerHTML = title[3].AGE[7]
+document.getElementById("h5").innerHTML = title[4].SEX[0]
+document.getElementById("h6").innerHTML = title[5].POS[6]
+document.getElementById("h7").innerHTML = title[6].DOE[7]
+
+document.getElementById("i1").innerHTML = title[0].NUM[8]
+document.getElementById("i2").innerHTML = title[1].FN[8]
+document.getElementById("i3").innerHTML = title[2].LA[8]
+document.getElementById("i4").innerHTML = title[3].AGE[8]
+document.getElementById("i5").innerHTML = title[4].SEX[1]
+document.getElementById("i6").innerHTML = title[5].POS[7]
+document.getElementById("i7").innerHTML = title[6].DOE[8]
+
+document.getElementById("j1").innerHTML = title[0].NUM[9]
+document.getElementById("j2").innerHTML = title[1].FN[9]
+document.getElementById("j3").innerHTML = title[2].LA[9]
+document.getElementById("j4").innerHTML = title[3].AGE[9]
+document.getElementById("j5").innerHTML = title[4].SEX[0]
+document.getElementById("j6").innerHTML = title[5].POS[8]
+document.getElementById("j7").innerHTML = title[6].DOE[9]
